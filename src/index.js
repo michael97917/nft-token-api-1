79ea6c68146a273e7bb323562bfc5b627bf590e1
johni1228 <akirus1228@gmail.com>
2021-12-04 12:40:38 +0000

    Init project for nft - API

--- src/index.js
+++ src/index.js
@@ -6,17 +6,29 @@ const app = express();
 const port = parseInt(process.env.PORT || 3000);
 const dataDir = process.env.DATA_DIR || 'public';
 const imageURI = process.env.IMAGE_URI || 'https://ipfs.io/ipfs';
-const imageHash = [];
+
+const imagePath = `${__dirname}/${dataDir}/result.json`;
+
 
 app.get('/api/v0/nfts/:nftId', function(req, res) {
+  let imagesURI;
+  try {
+    imagesURI = fs.readFileSync(imagePath);
+  } catch (error) {
+    console.log(error)
+  }
+
+  const imagesData = JSON.parse(imagesURI);
+
   const nftId = parseInt(req.params.nftId) + 1;
-  const jsonPath = `${dataDir}/json/${nftId}.json`;
+  const jsonPath = `${__dirname}/${dataDir}/json/${nftId}.json`;
+  console.log('jsonPath == ', jsonPath)
   try {
       const jsonBuffer = fs.readFileSync(jsonPath);
       const jsonString = jsonBuffer.toString();
       const jsonData = JSON.parse(jsonString);
       jsonData.external_url = `${imageURI}/images/${nftId}.png`;
-      jsonData.image = `${imageURI}/${imageHash[parseInt(req.params.nftId)]}`;
+      jsonData.image = `${imageURI}/${imagesData[`image${parseInt(req.params.nftId)}`]}`;
       return res.json(jsonData);
   } catch (e) {
       console.log(e);
